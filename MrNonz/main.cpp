15b1a1796474e1b7b79fd6fafbbf725b29189e2c
MrNonz-Windows <NONTAWAT39@GMAIL.COM>
2017-02-22 00:31:10 +0700

    init append_buffer and remove_buffer

--- MrNonz/main.cpp
+++ MrNonz/main.cpp
@@ -10,6 +10,7 @@ Lang C++
 #define producer_size 20
 #define consumer_size 30
 #define request_size 100000
+
 typedef struct thread_data
 {
     int head;
@@ -18,9 +19,13 @@ typedef struct thread_data
 }thread_data;
 
 //function prototype
-void initial_buffet(thread_data *);
+void initial_buffer(thread_data *);
+
 void add_item(thread_data *, char);
 void remove_item(thread_data *);
+
+void append_buffer(thread_data *, char);
+void remove_buffer(thread_data *);
 
 thread_data circular_queue;
 pthread_mutex_t lock_head, lock_tail;
@@ -36,17 +41,21 @@ int main()
     printf("Requests %d\n", request_size);
 
     //set default value of circular_queue
-    initial_buffet(&circular_queue);
+    initial_buffer(&circular_queue);
 
+    /*
     add_item(&circular_queue, 'a');
     add_item(&circular_queue, 'b');
     remove_item(&circular_queue);
 
     printf("Last head and tail address is %d %d", circular_queue.head, circular_queue.tail);
+    */
+    append_buffer(&circular_queue, 'a');
+    remove_buffer(&circular_queue);
     return 0;
 }
 
-void initial_buffet(thread_data *temp_buffer)
+void initial_buffer(thread_data *temp_buffer)
 {
     temp_buffer->head = 0;
     temp_buffer->tail = 0;
@@ -72,4 +81,14 @@ void remove_item(thread_data *temp_buffer)
     {
         temp_buffer->tail = 0;
     }
+}
+
+void append_buffer(thread_data *temp_buffer, char temp_data)
+{
+
+}
+
+void remove_buffer(thread_data *temp_buffer)
+{
+
 }
